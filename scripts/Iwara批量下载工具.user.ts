dd73330d26dad2de9f716ff7814ab916e7c2228b
dawn-lc <lc990930@163.com>
2022-03-27 12:15:15 +0800

    更新脚本信息

--- scripts/Iwara批量下载工具.user.ts
+++ scripts/Iwara批量下载工具.user.ts
@@ -1,33 +1,35 @@
 // ==UserScript==
-// @name           Iwara 批量下载工具
-// @name:en        Iwara Download Tool
-// @name:ja        Iwara バッチダウンローダー
-// @description    批量下载 Iwara 视频
-// @description:en Download videos from iwara.tv
-// @description:ja Iwara 動画バッチをダウンロード
-// @namespace      https://github.com/dawn-lc/user.js
-// @icon           https://iwara.tv/sites/all/themes/main/img/logo.png
-// @version        2.1.17
-// @author         dawn-lc
-// @license        Apache-2.0
-// @connect        iwara.tv
-// @match          *://*.iwara.tv/*
-// @grant          GM_getValue
-// @grant          GM_setValue
-// @grant          GM_listValues
-// @grant          GM_deleteValue
-// @grant          GM_addValueChangeListener
-// @grant          GM_removeValueChangeListener
-// @grant          GM_addStyle
-// @grant          GM_getResourceText
-// @grant          GM_download
-// @grant          GM_xmlhttpRequest
-// @grant          GM_openInTab
-// @grant          GM_info
-// @grant          GM_cookie
-// @grant          unsafeWindow
-// @require        https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/react/16.13.1/umd/react.production.min.js
-// @require        https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/react-dom/16.13.1/umd/react-dom.production.min.js
+// @name              Iwara Download Tool
+// @description       Download videos from iwara.tv
+// @name:ja           Iwara バッチダウンローダー
+// @description:ja    Iwara 動画バッチをダウンロード
+// @name:zh-CN        Iwara 批量下载工具
+// @description:zh-CN 批量下载 Iwara 视频
+// @namespace         https://github.com/dawn-lc/user.js
+// @source            https://github.com/dawn-lc/user.js
+// @supportURL        https://github.com/dawn-lc/user.js/issues
+// @icon              https://iwara.tv/sites/all/themes/main/img/logo.png
+// @version           2.1.18
+// @author            dawn-lc
+// @license           Apache-2.0
+// @connect           iwara.tv
+// @match             *://*.iwara.tv/*
+// @grant             GM_getValue
+// @grant             GM_setValue
+// @grant             GM_listValues
+// @grant             GM_deleteValue
+// @grant             GM_addValueChangeListener
+// @grant             GM_removeValueChangeListener
+// @grant             GM_addStyle
+// @grant             GM_getResourceText
+// @grant             GM_download
+// @grant             GM_xmlhttpRequest
+// @grant             GM_openInTab
+// @grant             GM_info
+// @grant             GM_cookie
+// @grant             unsafeWindow
+// @require           https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/react/16.13.1/umd/react.production.min.js
+// @require           https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/react-dom/16.13.1/umd/react-dom.production.min.js
 // ==/UserScript==
 (async function () {
     function UUID() {
