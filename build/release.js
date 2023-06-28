ee5553f1b7ea4dd250a0238637c1d4974ca13500
dawn-lc <lc990930@163.com>
2023-06-28 17:28:26 +0800

    迁移至NPM脚本编译

--- build/release.js
+++ build/release.js
@@ -8,5 +8,5 @@ const scriptMataPath = path.join(scriptsPath, 'mata.ts');
 
 let package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 
-fs.copyFileSync(path.join(root, 'temp', 'user.js'), path.join(root, 'dist', `${package.name}.user.js`));
-fs.copyFileSync(scriptMataPath, path.join(root, 'dist', `${package.name}.mata.js`));
+fs.copyFileSync(path.join(root, 'temp', 'user.js'), path.join(root, 'dist', `${package.displayName}.user.js`));
+fs.copyFileSync(scriptMataPath, path.join(root, 'dist', `${package.displayName}.mata.js`));
