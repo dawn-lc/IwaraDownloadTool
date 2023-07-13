1917fd9b14da97208c96f9e5b92c35b1bb36d75c
dawn-lc <lc990930@163.com>
2023-07-13 12:45:35 +0800

    PR #133

--- build/release.js
+++ build/release.js
@@ -2,16 +2,29 @@ const path = require('path');
 const fs = require('fs');
 const childProcess = require('child_process');
 const root = process.cwd();
+function mkdir(path) {
+    return fs.existsSync(path) || fs.mkdirSync(path)
+}
 
 function getGitBranch() {
     return childProcess.execSync('git rev-parse --abbrev-ref HEAD', { 'encoding': 'utf8' }).trim();
 }
 
 const packagePath = path.join(root, 'package.json');
-const scriptsPath = path.join(root, 'scripts');
-const scriptMataPath = path.join(scriptsPath, 'mata.ts');
+const tempPath = path.join(root, 'temp');
+const distPath = path.join(root, 'dist');
+const branchPath = path.join(distPath, getGitBranch());
+
+mkdir(tempPath);
+mkdir(distPath);
+mkdir(branchPath);
+const mainTempPath = path.join(tempPath, 'main.js');
+const mataTempPath = path.join(tempPath, 'mata.js');
 
 let package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 
-fs.copyFileSync(path.join(root, 'temp', 'user.js'), path.join(root, 'dist', getGitBranch(), `${package.displayName}.user.js`));
-fs.copyFileSync(scriptMataPath, path.join(root, 'dist', getGitBranch(), `${package.displayName}.mata.js`));
+let main = fs.readFileSync(mainTempPath, 'utf8');
+let mata = fs.readFileSync(mataTempPath, 'utf8');
+
+fs.writeFileSync(path.join(distPath, getGitBranch(), `${package.displayName}.user.js`), [mata, main].join('\n'));
+fs.writeFileSync(path.join(distPath, getGitBranch(), `${package.displayName}.mata.js`), mata);
