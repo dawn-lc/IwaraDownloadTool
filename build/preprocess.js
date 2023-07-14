b1011dc8e16a0353cc807ae3eb5fecadc0c6eef2
dawn-lc <lc990930@163.com>
2023-07-14 18:15:00 +0800

    fix 自动更新版本号BUG

--- build/preprocess.js
+++ build/preprocess.js
@@ -105,24 +105,11 @@ function serializeMetadata(metadata) {
 function mkdir(path) {
     return fs.existsSync(path) || fs.mkdirSync(path)
 }
-
-function addVersion(versionString) {
-    let version = versionString.split('.').map(Number);
-    version[version.length - 1] = version[version.length - 1] + 1;
-    return version.join('.');
-}
-
 const tempPath = path.join(root, 'temp');
 
 mkdir(tempPath);
 
-const packagePath = path.join(root, 'package.json');
 const sourcePath = path.join(root, 'src');
-
-let package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-
-package.version = addVersion(package.version);
-fs.writeFileSync(packagePath, JSON.stringify(package, null, 2));
 
 const mataTemplatePath = path.join(sourcePath, 'userjs.mata');
 const mataTempPath= path.join(tempPath, 'mata.js');
