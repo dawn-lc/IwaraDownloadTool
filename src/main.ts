c03b4bddb51894fced3160f6eb81dc2c884d5b59
dawn-lc <lc990930@163.com>
2023-12-21 15:20:18 +0800

    fix firefox 兼容性

--- src/main.ts
+++ src/main.ts
@@ -1151,7 +1151,7 @@
         border: 1px solid #ccc;
         border-radius: 5px;
         box-shadow: 0 0 10px #ccc;
-        transform: translate(90%, -50%);
+        transform: translate(85%, -50%);
         transition: transform 0.5s cubic-bezier(0,1,.60,1);
     }
     #pluginMenu ul {
